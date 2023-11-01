8bd5bddf0c5ae8b04d026dd98234b34df3dc939c
Franco <fj175504@gmail.com>
2023-11-01 11:32:12 -0500

    resultado

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function calcularSuma() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = num1 + num2;
+    var suma = num1 + num2;
+    
     document.getElementById("resultado").textContent = resultado;
   }
   
